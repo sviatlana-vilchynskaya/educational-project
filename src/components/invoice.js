9c274db916e013a014c12b2f0b5c5d4be9d02b8b
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2022-10-09 21:51:21 +0300

    feat: search parameter

--- src/components/invoice.js
+++ src/components/invoice.js
@@ -1,21 +1,46 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useSearchParams } from 'react-router-dom';
 import { getInvoices } from './data';
 
 export default function Invoices() {
   let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
   let activeClassName = `text-red-400 bg-gray-200`;
+
+  function onInputChange(event) {
+    let filter = event.target.value;
+    if (filter) {
+      setSearchParams({ filter });
+    } else {
+      setSearchParams({});
+    }
+  }
+
   return (
     <main className="text-center">
       <div className="flex flex-col gap-3">
-        {invoices.map((invoice) => (
-          <NavLink
-            className={({ isActive }) => (isActive ? activeClassName : '')}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+        <input
+          type="text"
+          className="border w-3/4 mx-auto mt-5"
+          placeholder="Search Invoice"
+          value={searchParams.get('filter') || ''}
+          onChange={onInputChange}
+        />
+        {invoices
+          .filter((invoice) => {
+            let filter = searchParams.get('filter');
+            if (!filter) return true;
+            let name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoice) => (
+            <NavLink
+              className={({ isActive }) => (isActive ? activeClassName : '')}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
       </div>
       <Outlet></Outlet>
     </main>
